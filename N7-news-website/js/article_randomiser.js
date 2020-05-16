cee84ce3f32d5d66782a852adea6046611e96b76
humble-barnacle001 <barnaclenshade@protonmail.com>
2020-05-16 23:32:35 +0530

    Article Page changes color on reload

---
+++ N7-news-website/js/article_randomiser.js
@@ -0,0 +1,18 @@
+const items = document.querySelectorAll("#article article div p");
+var r = 200 + Math.floor(Math.random() * 56);
+var g = 200 + Math.floor(Math.random() * 56);
+var b = 200 + Math.floor(Math.random() * 56);
+var tr = 255 - r;
+var tg = 255 - g;
+var tb = 255 - b;
+const run = () => {
+
+    var col = 'rgb('.concat(r.toString(), ',', g.toString(), ',', b.toString(), ')');
+    var tcol = 'rgb('.concat(tr.toString(), ',', tg.toString(), ',', tb.toString(), ')');
+    document.getElementById('article').style.backgroundColor = col;
+    items.forEach(item => {
+        item.style.color = tcol;
+        item.style.opacity = 1;
+    });
+};
+window.addEventListener('load', run);
