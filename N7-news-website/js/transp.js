d7d9d0b7e862356772d03909cd0c588872f35d2d
humble-barnacle001 <barnaclenshade@protonmail.com>
2020-05-17 13:40:27 +0530

    Final touches

--- N7-news-website/js/transp.js
+++ N7-news-website/js/transp.js
@@ -1,13 +1,19 @@
-if (window.innerWidth > 600) {
-    $(window).scroll({ previousTop: 0 },
-        function() {
-            var currentTop = $(window).scrollTop();
-            if (currentTop < this.previousTop) {
-                $(".navbar").slideDown();
-            } else {
-                $(".navbar").slideUp();
-            }
-            this.previousTop = currentTop;
+var previousTop = 0;
+const run = () => {
+
+    if ((window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) < 400) {
+        var st = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (st > lastScrollTop) {
+            document.getElementById('main-nav').style.transform = "translateY(-200px)";
+        } else {
+            document.getElementById('main-nav').style.transform = "translateY(0)";
         }
-    );
+        lastScrollTop = st <= 0 ? 0 : st;
+    }
 }
+window.addEventListener('scroll', run);
+
+
+
+var lastScrollTop = 0;
